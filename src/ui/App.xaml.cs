1f2e2a1cf39ff7784e055b89840a2e875699b112
Moskvová Jana Veronika (242206) <xmoskv01@vutbr.cz>
2024-03-26 16:51:41 +0100

    Added user interface for the calculator

---
+++ src/ui/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace calculadora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 349;
+            const int newHeight = 508;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            window.MinimumHeight = 450;
+            window.MaximumHeight = 1080;
+
+            window.MinimumWidth = 300;
+            window.MaximumWidth = 1920;
+
+            return window;
+        }
+    }
+}
