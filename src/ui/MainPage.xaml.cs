38615bbb95e7407fe7768171e8e28a683010be8d
Skypca77 <72163950+Skypca77@users.noreply.github.com>
2024-04-15 17:05:50 +0200

    Bug fixis of calculate function and litle fixis of colaboration betwean UI and mathlib

--- src/ui/MainPage.xaml.cs
+++ src/ui/MainPage.xaml.cs
@@ -18,51 +18,61 @@ namespace calculadora
         }
         void OnSquared(object sender, EventArgs e)
         {
-            result.Text = "^";
+            result.Text += "^";
         }
         void OnSquareRoot(object sender, EventArgs e)
-        {   
+        {
             result.Text += "√";
         }
         void OnNumberSelection0(object sender, EventArgs e)
         {
             result.Text += "0";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection1(object sender, EventArgs e)
         {
             result.Text += "1";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection2(object sender, EventArgs e)
         {
             result.Text += "2";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection3(object sender, EventArgs e)
         {
             result.Text += "3";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection4(object sender, EventArgs e)
         {
             result.Text += "4";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection5(object sender, EventArgs e)
         {
             result.Text += "5";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection6(object sender, EventArgs e)
         {
             result.Text += "6";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection7(object sender, EventArgs e)
         {
             result.Text += "7";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection8(object sender, EventArgs e)
         {
             result.Text += "8";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnNumberSelection9(object sender, EventArgs e)
         {
             result.Text += "9";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnDivision(object sender, EventArgs e)
         {
@@ -86,8 +96,8 @@ namespace calculadora
         }
         void OnRandom(object sender, EventArgs e)
         {
-            Random random = new Random(100);
-            result.Text += "" + random;
+            result.Text += "R";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnModulo(object sender, EventArgs e)
         {
@@ -95,7 +105,8 @@ namespace calculadora
         }
         void OnPi(object sender, EventArgs e)
         {
-            result.Text += "3.14159265359";
+            result.Text += "π";
+            result.Text = Mathlib.Operations.ridoff0(result.Text);
         }
         void OnComma(object sender, EventArgs e)
         {
@@ -103,7 +114,14 @@ namespace calculadora
         }
         void OnEquals(object sender, EventArgs e)
         {
-            result.Text = "" + Mathlib.Operations.calculate(result.Text);
+            try
+            {
+                result.Text = Mathlib.Operations.calculate(result.Text);
+            }
+            catch (Exception ex)
+            {
+                result.Text = ex.Message;
+            }
         }
     }
 }
